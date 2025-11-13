1928416ffa92ac0540913ca52f0cfaf371d74b52
tisonpatrik <patriktison@gmail.com>
2025-11-13 16:32:44 +0100

    migrations, proper db, pdadmin

--- live_dashboard/priv/repo/migrations/20251113152018_create_document_table.exs
+++ live_dashboard/priv/repo/migrations/20251113152018_create_document_table.exs
@@ -1,12 +1,15 @@
 defmodule LiveDashboard.Repo.Migrations.CreateDocumentTable do
   use Ecto.Migration
 
-  def change do
+  def up do
     create table("document", primary_key: false) do
       add :document_name, :string, size: 1024, null: false, primary_key: true
       add :content, :text, null: false
-      add :document_date, :date, null: false, primary_key: true
       add :created_at, :utc_datetime, default: fragment("CURRENT_TIMESTAMP")
     end
+  end
+
+  def down do
+    drop table("document")
   end
 end
