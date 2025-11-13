901e7c82e60552279fbad0f47702abc2ec5672cc
tisonpatrik <patriktison@gmail.com>
2025-11-13 16:32:44 +0100

    migrations, proper db, pdadmin

---
+++ live_dashboard/priv/repo/migrations/20251113160659_create_chunk_table.exs
@@ -0,0 +1,20 @@
+defmodule LiveDashboard.Repo.Migrations.CreateChunkTable do
+  use Ecto.Migration
+
+  def up do
+    execute "CREATE EXTENSION IF NOT EXISTS vector"
+
+    create table("chunk", primary_key: false) do
+      add :document_name, :string, size: 1024, null: false, primary_key: true
+      add :content, :text, null: false
+      add :embedding, :"vector(1536)"
+      add :labels, {:array, :smallint}
+      add :created_at, :utc_datetime, default: fragment("CURRENT_TIMESTAMP")
+    end
+  end
+
+  def down do
+    drop table("chunk")
+    execute "DROP EXTENSION IF EXISTS vector"
+  end
+end
