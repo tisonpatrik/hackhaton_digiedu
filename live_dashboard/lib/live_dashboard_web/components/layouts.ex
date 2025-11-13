a8d8c35a0d5769ccb3f2306787d75c8ba5e8606c
Zehra Sueda Gahramanli <zehra.gahramanli@doldurapp.com>
2025-11-13 17:58:29 +0100

    add dashboard

--- live_dashboard/lib/live_dashboard_web/components/layouts.ex
+++ live_dashboard/lib/live_dashboard_web/components/layouts.ex
@@ -73,6 +73,44 @@ defmodule LiveDashboardWeb.Layouts do
   end
 
   @doc """
+  Renders a full-width dashboard layout without max-width constraints.
+  """
+  attr :flash, :map, required: true, doc: "the map of flash messages"
+  slot :inner_block, required: true
+
+  def dashboard(assigns) do
+    ~H"""
+    <header class="navbar px-4 sm:px-6 lg:px-8">
+      <div class="flex-1">
+        <a href="/" class="flex-1 flex w-fit items-center gap-2">
+          <img src={~p"/images/logo.svg"} width="36" />
+          <span class="text-sm font-semibold">v{Application.spec(:phoenix, :vsn)}</span>
+        </a>
+      </div>
+      <div class="flex-none">
+        <ul class="flex flex-column px-1 space-x-4 items-center">
+          <li>
+            <a href="/" class="btn btn-ghost">Home</a>
+          </li>
+          <li>
+            <a href="/dashboard" class="btn btn-ghost">Dashboard</a>
+          </li>
+          <li>
+            <.theme_toggle />
+          </li>
+        </ul>
+      </div>
+    </header>
+
+    <main class="w-full">
+      {render_slot(@inner_block)}
+    </main>
+
+    <.flash_group flash={@flash} />
+    """
+  end
+
+  @doc """
   Shows the flash group with standard titles and content.
 
   ## Examples
